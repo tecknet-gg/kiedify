947e2b1286c36ef8b1f12a0c16a822b5fefaec16
Jeevan Hatti <hattijeevan@gmail.com>
2026-06-05 23:21:04 +0100

    isolator working

--- isolater.py
+++ isolater.py
@@ -1,4 +1,7 @@
 import subprocess
+import sys
+print(sys.executable)
+
 
 
 class Preprocessor:
@@ -6,16 +9,11 @@ class Preprocessor:
         pass
 
     def separateTrack(self, inputPath, outputDir):
-        subprocess.run([
-            "python", "-m", "demucs",
-            "-o", outputDir,
-            inputPath
-        ], check=True)
+        subprocess.run([sys.executable, "-m", "demucs", "-o", outputDir, inputPath], check=True)
 
 
 
 if __name__ == "__main__":
     inputPath = "/Users/jeevan/Documents/Python/MusicTTS/Music/Weezer/Weezer (Blue Album)/Say It Ain't So (Original Mix).mp3"
     outputDir = "/Users/jeevan/Documents/Python/MusicTTS/Music/Isolated"
-    preprocessor = Preprocessor()
-    preprocessor.separateTrack(inputPath, outputDir)
+
