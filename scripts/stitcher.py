359d7fb3441ea974d4c32338f0260af6ed2f0bbf
Jeevan Hatti <hattijeevan@gmail.com>
2026-06-11 22:50:58 +0100

    add stitcher class + update requirements.txt

---
+++ scripts/stitcher.py
@@ -0,0 +1,8 @@
+import pydub
+
+class Stitcher:
+    def __init__(self, dir="/Users/jeevan/Documents/Python/MusicTTS/Music"):
+        self.dir = dir
+
+    def generateMP3(self, stitchInstructions):
+        pass
