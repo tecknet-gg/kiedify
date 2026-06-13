dc98a5025cac34a9f57022e8597336d63cfe592a
Jeevan Hatti <hattijeevan@gmail.com>
2026-06-13 16:22:21 +0100

    current test setup

--- scripts/main.py
+++ scripts/main.py
@@ -1,40 +1,36 @@
-from downloader import Downloader
 from directory import DirectoryManager
+from downloader import Downloader
 from isolater import Preprocessor
+from lyrics import LyricFinder
+from searcher import Searcher
+from syncer import Syncer
+from stitcher import Stitcher
 
-import time
-import os
+manager = DirectoryManager()
+downloader = Downloader()
+preprocessor = Preprocessor()
+lyricFinder = LyricFinder()
+searcher = Searcher()
+syncer = Syncer()
+stitcher = Stitcher()
 
-'''
-if __name__ == "__main__":
-    #artists = ["Weezer", "Beatles", "Red Hot Chilli Peppers", "Paramore", "Avril Lavigne", "Laufey" ]
-    artists = []
-    if len(artists) == 0:
-        artist = input("Enter the artist: ")
-        numberAlbums = int(input("Enter the number of albums: "))
-        timeStart = time.perf_counter()
-        downloader = Downloader()
-        passed, failed = downloader.artistToInstalled(artist, qty=numberAlbums, nthreads=15)
-        timeEnd = time.perf_counter()
-    else:
-        timeStart = time.perf_counter()
-        downloader = Downloader()
-        passed, failed = downloader.queueArtists(artists, qty=10, nthreads=25)
-        timeEnd = time.perf_counter()
-
-    print(f"Download time: {round(timeEnd - timeStart, 2) } Successful: {passed} Failed: {failed}")
-    if passed!=0:
-        print(f"Time per song:{round((timeEnd - timeStart)/passed, 2)}")
-
-
-    processor = Preprocessor()
-    processor.generateQueue()
-    processor.processMulithreaded(nthread=4)
-    manager = DirectoryManager(musicDir)
+def aristPipeline(artist, qty=10):
+    albums = downloader.getDiscog(artist, qty=qty)
+    preprocessor.generateQueue()
+    preprocessor.processMulithreaded(nthread=4)
+    manager.flattenProcessedDir()
+    lyricFinder.generateQueue()
+    lyricFinder.gatherMulithreaded(nthread=5)
+    lyricFinder.cleanLyricless()
+    syncer.syncAll()
     manager.cleanIsolatedDir()
-    manager.flattenProcessedDir()
-'''
+    manager.cleanDownloadDir()
+    manager.cleanRawDir()
 
 if __name__ == "__main__":
-    manager = DirectoryManager()
-    manager.flattenProcessedDir()
+    print(f"Weezer, Red Hot Chilli Peppers, The Beatles, Paramore, Avril Lavigne, The Cardigans")
+    query = input("Enter the query: ")
+    artist = input("Enter the artist: ")
+
+    stitchList = searcher.getStitchMap(query, artist)
+    stitcher.generateMP3(stitchList)
