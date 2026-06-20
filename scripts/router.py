3a81763f45238b974bc61a5ee371872738d9f52c
Jeevan Hatti <hattijeevan@gmail.com>
2026-06-20 15:07:18 +0100

    Add method router class

---
+++ scripts/router.py
@@ -0,0 +1,42 @@
+import os
+from phoneme import PhonemeSynth, PhonemeExtractor, PhonemeNode
+from stitcher import Stitcher
+from searcher import Searcher
+
+class Router:
+    def __init__(self, globalNodes=None, semanticEngine=None, basicDB=None, weights=None):
+
+        self.globalNodes = globalNodes if globalNodes else None
+        self.semanticEngine = semanticEngine
+        self.basicDB = basicDB
+
+        self.extractor = PhonemeExtractor()
+        self.weights = weights if weights else self.extractor.phonemeWeights
+
+        self.synth = PhonemeSynth(self.globalNodes, weights=self.weights)
+        self.stitcher = Stitcher()
+        self.searcher = Searcher()
+
+    def basicMatch(self, text, artist, fuzzy=True, rtc=True):
+        if fuzzy:
+            stitchMap = self.searcher.basicMatch(text, artist, mode="fuzzy", rtc=True)
+        else:
+            stitchMap = self.searcher.basicMatch(text, artist, mode="exact")
+        file = self.stitcher.generateMP3(stitchMap)
+        print(f"Generated {file}")
+        return file
+
+    def semanticMatch(self, text, artist):
+        stitchMap = self.searcher.semanticMatch(text, artist)
+        file = self.stitcher.generateMP3(stitchMap)
+        print(f"Generated {file}")
+        return file
+
+    def phonemeMatch(self, text, artist):
+        stitchMap = self.synth.runCorpus(text)
+        file = self.stitcher.generateMP3(stitchMap)
+        print(f"Generated {file}")
+
+if __name__ == "__main__":
+    router = Router()
+    router.basicMatch("I love you", "Weezer", fuzzy=True)
