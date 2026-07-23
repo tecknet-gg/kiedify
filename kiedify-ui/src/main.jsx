c847d001c6eec2398a92a25cad78b51f4b4f9a16
Jeevan Hatti <hattijeevan@gmail.com>
2026-07-23 22:28:44 +0100

    Import Hackclub theme

--- kiedify-ui/src/main.jsx
+++ kiedify-ui/src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { ThemeProvider } from "theme-ui"
+import theme from "@hackclub/theme"
+import App from "./App.jsx"
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <ThemeProvider theme={theme}>
+            <App/>
+        </ThemeProvider>
+    </React.StrictMode>
 )
